317cf90ab4f3e49e774ad0512a107bd92277de25
shopifyexpertoffc-cmyk <shopifyexpertoffc@gmail.com>
2026-02-23 15:28:14 +0530

    Load admin routes via web routes file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,6 +26,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
 
+
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
